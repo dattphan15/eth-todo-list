b8b18ebcb03342980e1504a575d3963fc14061de
dattphan15 <ken.phan@live.ca>
2021-05-19 20:44:46 -0700

    create default task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+  // uint is an unsigned integer (can't be negative)
   uint public taskCount = 0;
+
+  // The task id, content, and checkbox
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  // creates an id for each task
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Learn how to use web3.js");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    // links tasks to an index
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
